70ae991f4fd7c796e02cdb2a17528f5230853ad6
Didanny <37907412+Didanny@users.noreply.github.com>
2018-10-17 15:19:43 +0300

    Add thrust audio

--- Project Boost/Assets/Rocket.cs
+++ Project Boost/Assets/Rocket.cs
@@ -7,10 +7,13 @@ public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
 
+    AudioSource audioSource;
+
     // Use this for initialization
     void Start ()
     {
-        rigidBody = GetComponent<Rigidbody>();       
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
     
     // Update is called once per frame
@@ -24,6 +27,11 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying) audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
